f8b52cca9b20d81e8c211cb7378c130de1f75bb1
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-10 20:42:57 -0300

    Implementando as restrições de movimento

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -14,26 +14,42 @@ namespace Xadrez_console
 
                 while (!partida.terminada)
                 {
+                    try
+                    {
+
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno: " + partida.turno);
+                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+
+                        Console.WriteLine();
+                        Console.Write("Posição de origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Posição de destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDestino(origem, destino);
+                        partida.realizaJogada(origem, destino);
+                    }
+
+                    catch (TabuleiroException e)
+                    {
+                        Console.Write(e.Message);
+                        Console.ReadLine();
+                    }
+
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
-                    Console.WriteLine();
-                    Console.Write("Posição de origem: ");
-                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
-                    
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
-
-                    Console.WriteLine();
-                    Console.Write("Posição de destino: ");
-                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
-
-                    partida.executaMovimento(origem, destino);
                 }
-
-                Tela.imprimirTabuleiro(partida.tab);
-
             }
 
             catch (TabuleiroException e)
