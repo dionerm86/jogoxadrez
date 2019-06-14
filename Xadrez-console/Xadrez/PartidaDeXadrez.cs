2e343b7eb778a4e938dd2968844bac0beab1b2ac
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-14 18:38:09 -0300

    Implementada a lógica de XequeMate

--- Xadrez-console/Xadrez/PartidaDeXadrez.cs
+++ Xadrez-console/Xadrez/PartidaDeXadrez.cs
@@ -70,8 +70,15 @@ namespace Xadrez
                 xeque = false;
             }
 
-            turno++;
-            mudaJogador();
+            if (testeXequeMate(adversaria(jogadorAtual)))
+            {
+                terminada = true;
+            }
+            else
+            {
+                turno++;
+                mudaJogador();
+            }
         }
 
         public void validarPosicaoDeOrigem(Posicao pos)
@@ -185,6 +192,40 @@ namespace Xadrez
             }
 
             return false;
+        }
+
+        public bool testeXequeMate(Cor cor)
+        {
+            if (!estaEmXeque(cor))
+            {
+                return false;
+            }
+
+            foreach (Peca x in pecasEmJogo(cor))
+            {
+                bool[,] mat = x.movimentosPossiveis();
+                for (int i = 0; i < tab.linhas; i++)
+                {
+                    for (int j = 0; j < tab.colunas; j++)
+                    {
+                        if (mat[i, j])
+                        {
+                            Posicao origem = x.posicao;
+                            Posicao destino = new Posicao(i, j);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
+                            bool testeXeque = estaEmXeque(cor);
+                            desfazMovimento(origem, destino, pecaCapturada);
+                            if (!testeXeque)
+                            {
+                                return false;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            return true;
         }
 
         public void colocarNovaPeca(char coluna, int linha, Peca peca)
